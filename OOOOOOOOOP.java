a734525a79660554b389914d2533324da63ff11e
Jamie Clarke <99755012+a-someone-person@users.noreply.github.com>
2025-01-24 15:15:32 +0000

    Start of Java OOP

---
+++ OOOOOOOOOP.java
@@ -0,0 +1,7 @@
+// a phone is a phone and a pen is a pen
+// a class has indstances which are referred to as objects
+class ooop{
+    public static void main(String[] args) {
+        //cool stuff happens here
+    }
+}
